136da72736245408ac23ab0a40a7d4f7847e2ca9
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2019-05-30 12:27:56 +0530

    Update traffic.cpp

--- traffic.cpp
+++ traffic.cpp
@@ -2,6 +2,7 @@
 #include<math.h>
 #include<bits/stdc++.h>
 #define size 75
+#include "movement.cpp"
 using namespace std;
 class road
 {
@@ -14,9 +15,9 @@ class road
         int cycle[4] = {1,20,0,0};
         int erick[4] = {2,25,0,0};
 
-        int vehicles,space ;
+        int vehicles,space,rows;
         double con;
-        char light;
+        char light , arr[25][3];
 
         road()
         {
@@ -25,11 +26,6 @@ class road
             //light = 'G';
         }
 
-        void total()
-        {
-            vehicles = truck[2]+bus[2]+car[2]+tempo[2]+erick[2]+bike[2]+cycle[2];
-            con = (truck[3]+bus[3]+car[3]+tempo[2]+erick[3]+bike[3]+cycle[3])/size;
-        }
         void Display()
         {
             if(truck[2]!=0)
@@ -46,6 +42,8 @@ class road
                 cout<<"\nNo. of bikes & scooties = "<<bike[2]<<" & space consumed = "<<bike[3]<<endl;
             if(cycle[2]!=0)
                     cout<<"\nNo. of cycles = "<<cycle[2]<<" & space consumed = "<<cycle[3]<<endl;  
+            
+            vehicles = truck[2]+bus[2]+car[2]+tempo[2]+erick[2]+bike[2]+cycle[2];
             cout<<"\nTotal Vehicles = "<<vehicles;
             cout<<"\nCongestion = "<<con;
         }
@@ -88,6 +86,7 @@ class road
 
 int main()
 {
+    srand(time(NULL));
     road r[4];
     int ch;
     for(int i=0;i<4;i++)
@@ -96,6 +95,8 @@ int main()
         cout<<"\n\t\tEnter the details for road "<<(i+1);
         cout<<"\n\tUnit of space occupied:- ";
         cin>>r[i].space;
+        r[i].con = double(r[i].space)/size;
+        
         do
         {
             cout<<"\n\tTruck...1";
@@ -113,41 +114,47 @@ int main()
                 cout<<"\nEnter the number of trucks:- ";
                 cin>>r[i].truck[2];
                 r[i].truck[3] = r[i].truck[2] * r[i].truck[0];
+                r[i].space-=r[i].truck[3];
                 break;
             case 2:
                 cout<<"\nEnter the number of buses:- ";
                 cin>>r[i].bus[2];
                 r[i].bus[3] = r[i].bus[2] * r[i].bus[0];
+                r[i].space-=r[i].bus[3];
                 break;
             case 3:
                 cout<<"\nEnter the number of cars :- ";
                 cin>>r[i].car[2];
                 r[i].car[3] = r[i].car[2] * r[i].car[0];
+                r[i].space-=r[i].car[3];
                 break;
             case 4:
                 cout<<"\nEnter the number of E-rickshaw:- ";
                 cin>>r[i].erick[2];
                 r[i].erick[3] = r[i].erick[2] * r[i].erick[0];
+                r[i].space-=r[i].erick[3];
                 break;
             case 5:
                 cout<<"\nEnter the number of Tempos:- ";
                 cin>>r[i].tempo[2];
                 r[i].tempo[3] = r[i].tempo[2] * r[i].tempo[0];
+                r[i].space-=r[i].tempo[3];
                 break;
             case 6:
                 cout<<"\nEnter the number of Bikes & scooty:- ";
                 cin>>r[i].bike[2];
                 r[i].bike[3] = r[i].bike[2] * r[i].bike[0];
+                r[i].space-=r[i].bike[3];
                 break;
             case 7:
                 cout<<"\nEnter the number of Cycles:- ";
                 cin>>r[i].cycle[2];
                 r[i].cycle[3] = r[i].cycle[2] * r[i].cycle[0];
+                r[i].space-=r[i].cycle[3];
                 break;
             }
-            cout<<"\nDo you want to enter more? Yes...1 No...0 ";
-            cin>>ch;
-        } while (ch==1);
+            
+        } while (r[i].space>0);
     }
 
     for(int i=0;i<4;i++)
@@ -156,14 +163,47 @@ int main()
         r[i].Display();
     }
 
-    int source , destination,l,rows;
+    /*int source , destination,l;
     cout<<"\n\nEnter source & destination :- ";
-    cin>>source>>destination;
+    cin>>source>>destination;*/
+    int l;
 
     cout<<"\nEnter the total no. of psuedo-lanes (not more than 3):- ";
     cin>>l;
+    
+    cout<<"\nTest 0";
+    for(int i=0;i<4;i++)
+    {
+        cout<<"\nTest 1";
+        r[i].rows = (int)(ceil(r[i].vehicles/l));
+        define(r[i].arr);
+        
+        cout<<"\nTest 2";
+        if(r[i].truck[2]>0)
+            arrange(r[i].truck[2] , 'T', r[i].arr , r[i].rows);
+        cout<<"\nTest 3";
+        if(r[i].bus[2]>0)
+            arrange(r[i].bus[2] , 'B', r[i].arr , r[i].rows);
+        cout<<"\nTest 4";
+        if(r[i].car[2]>0)
+            arrange(r[i].car[2] , 'C', r[i].arr , r[i].rows);
+        cout<<"\nTest 5";
+        if(r[i].tempo[2]>0)
+            arrange(r[i].tempo[2] , 't', r[i].arr , r[i].rows);
+        cout<<"\nTest 6";
+        if(r[i].erick[2]>0)
+            arrange(r[i].erick[2] , 'E', r[i].arr , r[i].rows);
+        cout<<"\nTest 7";
+        if(r[i].bike[2]>0)
+            arrange(r[i].bike[2] , 'b', r[i].arr , r[i].rows);
+        cout<<"\nTest 8";
+        if(r[i].cycle[2]>0)
+         arrange(r[i].cycle[2] , 'c', r[i].arr , r[i].rows);
 
-    rows = (int)(ceil(r[0].vehicles/l);
+        cout<<"\n\tArrangement of road "<<(i+1)<<" is :- "<<endl;
+        display(r[i].arr , r[i].rows);
+    }
+    
 
     /*Signal s(r);
     s.DispQueue();*/
