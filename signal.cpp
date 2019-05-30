d52049e0e0b0f0343763da38c692ade26a2be48e
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2019-05-30 15:48:33 +0530

    Add files via upload

---
+++ signal.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+    int dur;
+   
+    /*void Signal(char arr[25][3])
+    {
+        dur=60;
+        for(int i=0;i<4;i++)
+        {
+            v.insert(i+1 , r[i].con , r[i].light);
+        }
+    }*/
+
+    /*string Maximum(int t , int c , int bus , int b , int cycle ,int tempo , int e)
+    {
+        if((t>c && t>=bus && t>b && t>cycle && t>tempo && t>e ) || (bus>t))
+            return "truck";
+        if()
+    }*/
+
+    
+    /*void Congestion( int vehicles )
+    {
+        if()
+        
+    }*/
+
